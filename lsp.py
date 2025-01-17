045c2619108e2e119348a0145ad87ebf63b06e6a
Alex62-Creator <cham62ss@gmail.com>
2025-01-17 06:17:40 +0300

    add code from lesson

---
+++ lsp.py
@@ -0,0 +1,16 @@
+class Bird:
+    def fly(self):
+        print("эта птица летает")
+
+class Duck(Bird):
+    def fly(self):
+        print("Эта утка летает быстро")
+
+def fly_in_the_sky(animal):
+    animal.fly()
+
+b = Bird()
+d = Duck()
+
+fly_in_the_sky(b)
+fly_in_the_sky(d)
